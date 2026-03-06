b22aead21e587cffade00ec73df08a70d74db27d
Saint Bozo Officiel <sekoubtieta83@gmail.com>
2026-03-06 16:40:54 +0000

    Ajoute un formulaire Email et Mot de passe à ma page de connexion. Utili

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,10 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   images: {
