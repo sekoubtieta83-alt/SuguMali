5218e45c1b9aeceafcc442a45d097179d6a60ac8
Saint Bozo Officiel <sekoubtieta83@gmail.com>
2026-03-08 23:50:32 +0000

    supprime aussi les caches

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,11 @@
-
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
 admin.initializeApp();
 
-// Fonction de santé avec réponse JSON correcte
+/**
+ * Fonction de santé du backend SuguMali.
+ */
 export const healthCheck = functions.https.onRequest((request, response) => {
   response.json({
     status: 'online',
