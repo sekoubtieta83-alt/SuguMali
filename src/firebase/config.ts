d4f0991567d7ff680c1905699f1abb78260cb083
Saint Bozo Officiel <sekoubtieta83@gmail.com>
2026-03-27 22:53:36 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyB334gb71yjX0yMstR7CjBmB4GMdF9Bak0",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "studio-5667457400-e2c8f.firebaseapp.com",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "studio-5667457400-e2c8f",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "studio-5667457400-e2c8f.firebasestorage.app",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "979371818437",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:979371818437:web:32496cc33253cd55ee9fac"
+  "projectId": "studio-5667457400-e2c8f",
+  "appId": "1:979371818437:web:32496cc33253cd55ee9fac",
+  "apiKey": "AIzaSyB334gb71yjX0yMstR7CjBmB4GMdF9Bak0",
+  "authDomain": "studio-5667457400-e2c8f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "979371818437"
 };
