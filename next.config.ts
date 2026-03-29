17c851bc0efa299e698007fdaf6110d7a92ed77c
Saint Bozo Officiel <sekoubtieta83@gmail.com>
2026-03-29 05:01:51 +0000

    Fix: COOP header + firebase direct init

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,24 @@
-
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+        ],
+      },
+    ];
   },
   images: {
     remotePatterns: [
